fc992d444c7b37b4d4cfa1a68188556404e464a8
roimahns <109640106+roimahns@users.noreply.github.com>
2024-01-15 17:21:13 -0400

    Added images, and updated profile

--- app/pages/Profile.js
+++ app/pages/Profile.js
@@ -1,18 +1,42 @@
 import * as React from "react";
-import { View, useWindowDimensions, Text } from "react-native";
-import { TabView, SceneMap } from "react-native-tab-view";
+import { View, useWindowDimensions } from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 
 const FirstRoute = () => (
-  <View style={{ flex: 1, backgroundColor: "#ff4081" }} />
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: "#fff",
+      marginLeft: 30,
+      marginRight: 30,
+    }}
+  />
 );
-
 const SecondRoute = () => (
-  <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: "#fff",
+      marginLeft: 30,
+      marginRight: 30,
+    }}
+  />
+);
+const ThirdRoute = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: "#fff",
+      marginLeft: 30,
+      marginRight: 30,
+    }}
+  />
 );
 
 const renderScene = SceneMap({
   first: FirstRoute,
   second: SecondRoute,
+  third: ThirdRoute,
 });
 
 export default function TabViewExample() {
@@ -20,18 +44,27 @@ export default function TabViewExample() {
 
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: "first", title: "First" },
-    { key: "second", title: "Second" },
+    { key: "first", title: "Academics" },
+    { key: "second", title: "Extracurriculars" },
+    { key: "third", title: "Honors" },
   ]);
 
   return (
     <>
-      <Text>Profile!</Text>
       <TabView
         navigationState={{ index, routes }}
         renderScene={renderScene}
         onIndexChange={setIndex}
         initialLayout={{ width: layout.width }}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            indicatorStyle={{ backgroundColor: "black" }}
+            style={{ backgroundColor: "#F7B500", justifyContent: "center" }}
+            labelStyle={{ fontWeight: "bold" }}
+            contentContainerStyle={{ justifyContent: "center", flexGrow: 1 }}
+          />
+        )}
       />
     </>
   );
