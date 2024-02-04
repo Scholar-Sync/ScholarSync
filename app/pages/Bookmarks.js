a0353ce6ea1ca15e8c9d0b5fb303e4cbcc4bbf27
roimahns <109640106+roimahns@users.noreply.github.com>
2024-02-04 14:04:13 -0400

    updated

--- app/pages/Bookmarks.js
+++ app/pages/Bookmarks.js
@@ -53,7 +53,9 @@ const BookmarksScreen = () => {
       setFilteredData(bookmarksData);
     } else {
       const filtered = bookmarksData.filter((item) =>
-        `${item.name} ${item.grade} ${item.school}`.toLowerCase().includes(text.toLowerCase())
+        `${item.name} ${item.grade} ${item.school}`
+          .toLowerCase()
+          .includes(text.toLowerCase())
       );
       setFilteredData(filtered);
     }
